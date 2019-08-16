667a3b2de094c96b15f15cfc08ce3381ac692df2
Jack Crescend <jack.crescend@gmail.com>
2019-08-16 13:55:51 +0300

    Further removals

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const fs = require('fs');
-const { prefix, token } = require('./config.json');
+const token = require('./config.json');
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+client.emotes = new Discord.Collection();
 
 client.once('ready', () => {
+
+    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+    for (file of commandFiles) {
+        const command = require(`./commands/${file}`);
+        client.commands.set(command.name, command);
+    }
+
     console.log('Ready!');
 });
 
