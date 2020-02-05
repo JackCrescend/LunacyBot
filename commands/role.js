42dcb5c80414b0f4b87e801ad66cdfd3bcd3250f
Jack Crescend <jack.crescend@gmail.com>
2020-02-05 16:47:16 +0200

    Role usage manual added

--- commands/role.js
+++ commands/role.js
@@ -1,4 +1,19 @@
-const Discord = require('discord.js');
+const help_message = 
+`**Commands to toggle color roles**
+
+<:yellowEmote:674356833766473744> = !role yellow
+<:orangeEmote:674356833770668043> = !role orange
+<:redEmote:674356835905568769> = !role red
+<:pinkEmote:674356833762410517> = !role pink
+<:magentaEmote:674356833443512346> = !role magenta
+<:purpleEmote:674356833393311837> = !role purple
+<:blueEmote:674356833707753480> = !role blue
+<:cyanEmote:674356833779187772> = !role cyan
+<:turquoiseEmote:674356833783382058> = !role turquoise
+<:greenEmote:674356833762148372> = !role green
+<:blackEmote:674356833766604810> = !role black
+<:grayEmote:674356833384792086> = !role gray
+<:whiteEmote:674356833779056640> = !role white`;
 
 const guilds_roles = new Map();
 
@@ -15,6 +30,7 @@ const lounge_id = "674290040486232065";
 const lounge_roles = new Map();
 lounge_roles.set("green", "674347563062788109");
 lounge_roles.set("gray", "674291144859058215");
+lounge_roles.set("grey", "674291144859058215");
 lounge_roles.set("black", "674351017537568794");
 lounge_roles.set("white", "674351204758716446");
 lounge_roles.set("blue", "674349925756174367");
@@ -30,10 +46,14 @@ guilds_roles.set(lounge_id, lounge_roles);
 
 module.exports = {
     name: "role",
-    alias: [],
+    alias: ["toggle"],
     execute(client, message, args) {
         if (args.length != 1) { return; }
         const rolename = args[0].toLowerCase();
+
+        if (rolename == "help") {
+            message.channel.send(help_message);
+        }
 
         if (guilds_roles.has(message.guild.id)) {
             const available_roles = guilds_roles.get(message.guild.id);
