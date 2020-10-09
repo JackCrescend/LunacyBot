8ffbb5e74d2451f962f5d8f0688231b961fbd670
Jack Crescend <jack.crescend@gmail.com>
2020-10-09 18:40:16 +0300

    Refactored time command to use external API

--- commands/time.js
+++ commands/time.js
@@ -1,129 +1,41 @@
+const timezones = require("../timezone.json").map(timezone => timezone.toLowerCase());
+const fetch = require("node-fetch");
+const URL = "http://worldtimeapi.org/api/timezone";
+
 module.exports = {
     name: "time",
-    alias: ["servertime", "st"],
+    alias: ["timezone"],
     hidden: false,
-    parameters: ["[timezone] = Optional, the target timezone (GMT, UTC+4, PST etc..), defaults to UTC"],
-    info: "Gives current time in given timezone, the list of timezones currently hard-coded and contains hopefully most of the important ones",
-    execute(client, message, args) {
-        const time = new Date();
-        const currentHours = time.getUTCHours();
-        const currentMinutes = time.getUTCMinutes();
-        let offset = 0;
+    parameters: [],
+    info: "",
+    async execute (client, message, args) {
+        if (args.length < 1) {
+            message.reply("give a timezone or a capital name as parameter");
+            return;
+        }
+        const searchTerm = args.map(word => word.toLowerCase()).join("_");
+        const timezone = timezones.find(t => t.includes(searchTerm));
 
-        if (args.length > 0) {
-            const timezone = args[0].toUpperCase();
-
-            if (timezone == "SERVERTIME" || timezone == "ST" || timezone == "GMT" || timezone == "UTC") {
-                offset = 0;
-            } else if (timezone == "GMT+1" || timezone == "UTC+1" || timezone == "CET" || timezone == "BST" || timezone == "BDT") {
-                offset = 1;
-            } else if (timezone == "GMT+2" || timezone == "UTC+2" || timezone == "CEST" || timezone == "GC" || timezone == "GRANDCHASE") {
-                offset = 2;
-            } else if (timezone == "GMT+3" || timezone == "UTC+3") {
-                offset = 3;
-            } else if (timezone == "GMT+4" || timezone == "UTC+4") {
-                offset = 4;
-            } else if (timezone == "GMT+5" || timezone == "UTC+5") {
-                offset = 5;
-            } else if (timezone == "GMT+6" || timezone == "UTC+6") {
-                offset = 6;
-            } else if (timezone == "GMT+7" || timezone == "UTC+7") {
-                offset = 7;
-            } else if (timezone == "GMT+8" || timezone == "UTC+8") {
-                offset = 8;
-            } else if (timezone == "GMT+9" || timezone == "UTC+9") {
-                offset = 9;
-            } else if (timezone == "GMT+10" || timezone == "UTC+10") {
-                offset = 10;
-            } else if (timezone == "GMT+11" || timezone == "UTC+11") {
-                offset = 11;
-            } else if (timezone == "GMT+12" || timezone == "UTC+12") {
-                offset = 12;
-            } else if (timezone == "GMT-1" || timezone == "UTC-1") {
-                offset = -1;
-            } else if (timezone == "GMT-2" || timezone == "UTC-2") {
-                offset = -2;
-            } else if (timezone == "GMT-3" || timezone == "UTC-3") {
-                offset = -3;
-            } else if (timezone == "GMT-4" || timezone == "UTC-4") {
-                offset = -4;
-            } else if (timezone == "GMT-5" || timezone == "UTC-5" || timezone == "EST") {
-                offset = -5;
-            } else if (timezone == "GMT-6" || timezone == "UTC-6" || timezone == "CST") {
-                offset = -6;
-            } else if (timezone == "GMT-7" || timezone == "UTC-7" || timezone == "PDT") {
-                offset = -7;
-            } else if (timezone == "GMT-8" || timezone == "UTC-8" || timezone == "PST") {
-                offset = -8;
-            } else if (timezone == "GMT-9" || timezone == "UTC-9") {
-                offset = -9;
-            } else if (timezone == "GMT-10" || timezone == "UTC-10") {
-                offset = -10;
-            } else if (timezone == "GMT-11" || timezone == "UTC-11") {
-                offset = -11;
-            } else if (timezone == "GMT-12" || timezone == "UTC12") {
-                offset = -12;
-            } else {
-                let response = "Timezone not recognized, sorry!";
-                message.reply(response);
-                return;
-            }
-            
+        if (!timezone) {
+            message.reply(`could not find timezone ${timezone}`);
+            return;
         }
 
-        let hours = currentHours + offset;
-        let minutes = currentMinutes;
-        let PM = false;
-
-        if (hours >= 24) {
-            hours -= 24;
-        } else if (hours < 0) {
-            hours += 24;
+        const response = await fetch(URL + "/" + timezone);
+        if (!response.ok) {
+            console.log(response);
+            message.reply("timezone API call failed :<");
+            return;
         }
 
-        if (hours >= 12) {
-            PM = true;
-        }
-
-        let emote = " ";
-        if (hours >= 0 && hours < 8) {
-            emote += client.comboemotes.get("kannasleep");
-        } else if (hours >= 8 && hours < 12) {
-            emote += client.comboemotes.get("kannasippy");
-        } else if (hours >= 12 && hours < 18) {
-            emote += client.comboemotes.get("tohruhi"); 
-        } else if (hours >= 18 && hours < 24) {
-            emote += client.comboemotes.get("smugpls");
-        }
-
-        if (hours == 0) {
-            hours += 12;
-        } else if (hours >= 13) {
-            hours -= 12;
-        }
-
-        let response = "Current time ";
-        if (args.length > 0) {
-            response += `in ${args[0]} => `;
-        } else {
-            response += "in UTC => ";
-        }
-
-        response += hours + ":";
-
-        if (minutes < 10) {
-            response += "0";
-        }
-        response += minutes + " ";
-
-        if (PM) {
-            response += "PM";
-        } else {
-            response += "AM";
-        }
-
-        response += emote;
-
-        message.channel.send(response);
+        const data = await response.json();
+        const currentTime = data.datetime.substring(11, 19);
+        let reply = `Current time in ${data.timezone} (${data.abbreviation}): ${currentTime} `;
+        const hour = parseInt(currentTime.substring(0,2), 10);
+        if (hour < 8) reply += client.comboemotes.get("kannasleep");
+        else if (hour < 12) reply += client.comboemotes.get("kannasip");
+        else if (hour < 18) reply += client.comboemotes.get("tohruhi");
+        else reply += client.comboemotes.get("cerberusawa");
+        message.channel.send(reply);
     }
 };
